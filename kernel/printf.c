0ae0a22b7d40f7dc1e1ad5b0ffb7756bdae10013
JahJahWay <qiangjian71@gmail.com>
2023-01-18 12:48:06 +0800

    feat: Traps: Backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,17 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+    uint64 fp = r_fp();
+    uint64 top = PGROUNDUP(fp);
+    while (1) {
+        if (fp == top) break;
+
+        printf("%p\n", (uint64*)(fp - 8));
+
+        fp = (uint64*)(fp - 16);
+    }
+}
